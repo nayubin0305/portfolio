7af1064c5b4df132b83e65fe5d708bad558d4980
나유빈 <129129067+nayubin0305@users.noreply.github.com>
2026-09-11 17:58:43 +0900

    refactor: 조말론 프로젝트 커스텀 알럿창 스크립트 개선 및 링크 요소 시맨틱 마크업 정리

--- 01jomalone/com.js
+++ 01jomalone/com.js
@@ -58,12 +58,44 @@ $(".ham-btn").mouseleave(function(){
 });
 
 // 2026-09-10 디자인 알럿창 추가
+// 2026-09-11 디자인 알럿창 활용도 향상
 $(function() {
     if ($(".alert").length === 0) {
         $("body").append("<div class='alert'></div>");
     }
 });
+
+var alertTimer;
+
+function showAlert(message) {
+    clearTimeout(alertTimer);
+    $(".alert").stop().text(message).addClass("on");
+
+    alertTimer = setTimeout(function() {
+        $(".alert").removeClass("on");
+    }, 1800);
+}
+
+// 알럿 : 준비중
 $(".soon").click(function(e) {
     e.preventDefault();
-    $(".alert").stop().text("준비중 입니다.").fadeIn().delay(1500).fadeOut();
+    showAlert("준비중 입니다.");
+});
+
+// 알럿 : 복사
+$(".item .btn-clone").click(function(){
+    showAlert("상품 링크가 복사 완료");
+});
+
+// 알럿 : 위시리스트
+$(".item .btn-heart").click(function(){
+    var heart = $(this).parents(".item");
+
+    if (heart.hasClass("on")) {
+        heart.removeClass("on");
+        showAlert("위시리스트 등록 해제");
+    } else {
+        heart.addClass("on");
+        showAlert("위시리스트 등록");
+    }
 });
