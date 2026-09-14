6ba1e0d9f4ef887ac5ed44f964591b2778e44678
나유빈 <129129067+nayubin0305@users.noreply.github.com>
2026-09-14 21:19:52 +0900

    feat: 젠틀몬스터 프로젝트 정렬 필터 기능 추가 및 전반적인 접근성/마크업 개선

--- 02gentlemonster/com.js
+++ 02gentlemonster/com.js
@@ -1,188 +1,138 @@
-// 해더 메뉴바
+// 헤더 메뉴바
 $("header .menu-li").mouseenter(function(){
-    $(this).find(".sub").stop().slideDown()
-})
+    $(this).find(".sub").stop().slideDown();
+});
 $("header .menu-li").mouseleave(function(){
-    $(this).find(".sub").stop().slideUp()
-})
+    $(this).find(".sub").stop().slideUp();
+});
 
-var last_sc=0
-// 문서시작하자마자
-spread_y()
-history_line()
-header_white()
-mask_l()
-mask_r()
-mask_t()
-// 스크롤이 움직일 때
-$(window).scroll(function(){
-    spread_y()
-    history_line()
-    header_white()
-    header_hidden()
-    mask_l()
-    mask_r()
-    mask_t()
-    
-})
-// 히스토리 이미지박스 스크립트
-function spread_y(){
-    win_h = $(window).height()
-    sc = $(window).scrollTop()
+// 2026-09-10 중복 스크립트 정리
+var lastScroll = $(window).scrollTop();
+
+// 클립패스 마스크 함수
+var maskConfig = {
+    ".item-img": "inset(0 0 100% 0)",
+    ".mask-l": "inset(0 100% 0 0)",
+    ".mask-r": "inset(0 0 0 100%)",
+    ".mask-t": "inset(0 0 100% 0)"
+};
+
+function checkMasks(){
+    var winH = $(window).height();
+    var sc = $(window).scrollTop();
+
+    $.each(maskConfig, function(selector, closedValue){
+        $(selector).each(function(){
+            var itemTop = $(this).offset().top;
+            if(itemTop < sc + winH - 200){
+                $(this).css({ clipPath: "inset(0 0 0 0)" });
+            } else {
+                $(this).css({ clipPath: closedValue });
+            }
+        });
+    });
+}
+
+//히스토리 라인 길이 함수
+function historyLine(){
+    var win_h = $(window).height();
+    var sc = $(window).scrollTop();
+
     $(".item-img").each(function(inx){
-        this_top = $(this).offset().top;
-        console.log(this_top)
+        var this_top = $(this).offset().top;
+        var this_height = $(this).height()+100;
         if(this_top < sc + win_h - 200){
-            $(this).css({clipPath: "inset(0 0 0 0)"})
-        }else{
-            $(this).css({clipPath: "inset(0 0 100% 0)"})
+           $(".line .color").height(inx * this_height + 100);
         }
-    })
-}
-function mask_l(){
-    win_h = $(window).height()
-    sc = $(window).scrollTop()
-    $(".mask-l").each(function(inx){
-        this_top = $(this).offset().top;
-        if(this_top < sc + win_h - 200){
-            $(this).css({clipPath: "inset(0 0 0 0)"})
-        }else{
-            $(this).css({clipPath: "inset(0 100% 0 0)"})
-        }
-    })
-}
-function mask_r(){
-    win_h = $(window).height()
-    sc = $(window).scrollTop()
-    $(".mask-r").each(function(inx){
-        this_top = $(this).offset().top;
-        if(this_top < sc + win_h - 200){
-            $(this).css({clipPath: "inset(0 0 0 0)"})
-        }else{
-            $(this).css({clipPath: "inset(0 0 0 100%)"})
-        }
-    })
-}
-function mask_t(){
-    win_h = $(window).height()
-    sc = $(window).scrollTop()
-    $(".mask-t").each(function(inx){
-        this_top = $(this).offset().top;
-        if(this_top < sc + win_h - 200){
-            $(this).css({clipPath: "inset(0 0 0 0)"})
-        }else{
-            $(this).css({clipPath: "inset(0 0 100% 0)"})
-        }
-    })
+    });
 }
 
-//라인 길이 함수
-function history_line(){
-    win_h = $(window).height()
-    sc = $(window).scrollTop()
-    $(".item-img").each(function(inx){
-        this_top = $(this).offset().top;
-        this_height = $(this).height()+100
-        console.log(this_top)
-        if(this_top < sc + win_h - 200){
-           $(".line .color").height(inx*this_height+100)
-        }else{
-            
-        }
-    })
-}
+//해더 보임/숨김
+function headerState(){
+    var sc = $(window).scrollTop();
 
-//해더 흰배경 클라스 더하기 함수
-function header_white(){
-    sc = $(window).scrollTop()
-    if(sc>100){
-        $("header").addClass("black-bg")
-    }else{
-        $("header").removeClass("black-bg")
+    $("header").toggleClass("black-bg", sc > 100);
+
+    if(lastScroll - sc < 0){
+        $("header").css({ transform: "translateY(-100%)" });
+    } else {
+        $("header").css({ transform: "translateY(0)" });
     }
-}
-
-//해더스크롤 내릴때 스크롤 올릴때 함수
- // last_sc 변수를 먼저 구하고 실핼시킬 것
- last_sc = $(window).scrollTop()
-function header_hidden(){
-    sc = $(window).scrollTop()
-    if(last_sc-sc < 0){ //스크롤을 아래로 움직였을때
-        $("header").css({top:-80})
-    }else{
-        $("header").css({top:0})
-    }
-    last_sc = sc
+    lastScroll = sc;
 }
 
 // 탑버튼
-var sc = 0
-    if(sc==0){
-        $(".top-btn").stop().fadeOut()
-        $(".qna-btn").stop().fadeOut()
-    }else{
-        $(".top-btn").stop().fadeIn()
-        $(".qna-btn").stop().fadeIn()
+function toggleFloatingButtons(){
+    var sc = $(window).scrollTop();
+    var $buttons = $(".top-btn, .qna-btn");
+    if(sc === 0){
+        $buttons.stop().fadeOut();
+    } else {
+        $buttons.stop().fadeIn();
     }
+}
 
-$(window).scroll(function(){
-    sc = $(window).scrollTop()
-    if(sc==0){
-        $(".top-btn").stop().fadeOut()
-        $(".qna-btn").stop().fadeOut()
-    }else{
-        $(".top-btn").stop().fadeIn()
-        $(".qna-btn").stop().fadeIn()
-    }
- })
+function onScroll(){
+    checkMasks();
+    historyLine();
+    headerState();
+    toggleFloatingButtons();
+}
+
+// 초기 실행
+onScroll();
+
+$(window).scroll(onScroll);
 
 //  사이트맵
 $(".ham-btn").click(function(){
-    $(".ham-btn").toggleClass("active")
-    $(".site-map").toggleClass("active")
- })
+    var isOpen = $(this).attr("aria-expanded") === "true";
+    $(this).attr("aria-expanded", !isOpen);
+
+    $(".ham-btn").toggleClass("active");
+    $(".site-map").toggleClass("active");
+});
 
  $(".site-map .menu-li").click(function(){
-    menu_=$(this).index()
-    $(".site-map .sub").eq(menu_).stop().slideToggle()
-    $(".site-map .sub").not($(".site-map .sub").eq(menu_)).stop().slideUp()
- })
+    var idx = $(this).index();
+    $(".site-map .sub").eq(idx).stop().slideToggle();
+    $(".site-map .sub").not($(".site-map .sub").eq(idx)).stop().slideUp();
+});
 
- // 커서
+// 커서
 $(window).mousemove(function(event){
-    _x = event.clientX
-    _y = event.clientY
-    $(".cursor").css({left:_x, top:_y})
-})
-$(".pick_").mouseenter(function(){
-$(".cursor").addClass("over")
-})
-$(".pick_").mouseleave(function(){
-    $(".cursor").removeClass("over")
-})
+    $(".cursor").css({ left: event.clientX, top: event.clientY });
+});
 
-$(".pick-b").mouseenter(function(){
-$(".cursor").addClass("over")
-})
-$(".pick-b").mouseleave(function(){
-    $(".cursor").removeClass("over")
-})
+var cursorHoverTargets = ".pick_, .pick-a, .pick-b, header .menu-li, .ham-btn";
 
-$(".pick-a").mouseenter(function(){
-$(".cursor").addClass("over")
-})
-$(".pick-a").mouseleave(function(){
-    $(".cursor").removeClass("over")
-})
-$("header .menu-li").mouseenter(function(){
-$(".cursor").addClass("over")
-})
-$("header .menu-li").mouseleave(function(){
-    $(".cursor").removeClass("over")
-})
-$(".ham-btn").mouseenter(function(){
-$(".cursor").addClass("over")
-})
-$(".ham-btn").mouseleave(function(){
-    $(".cursor").removeClass("over")
-})
+$(cursorHoverTargets).mouseenter(function(){
+    $(".cursor").addClass("over");
+});
+$(cursorHoverTargets).mouseleave(function(){
+    $(".cursor").removeClass("over");
+});
+
+// 2026-09-10 디자인 알럿창 추가
+$(function() {
+    if ($(".alert").length === 0) {
+        $("body").append("<div class='alert'></div>");
+    }
+});
+
+var alertTimer;
+
+function showAlert(message) {
+    clearTimeout(alertTimer);
+    $(".alert").stop().text(message).addClass("on");
+
+    alertTimer = setTimeout(function() {
+        $(".alert").removeClass("on");
+    }, 1800);
+}
+
+// 알럿 : 준비중
+$(".soon").click(function(e) {
+    e.preventDefault();
+    showAlert("준비중 입니다.");
+});
--- 02gentlemonster/index.js
+++ 02gentlemonster/index.js
@@ -1,4 +1,6 @@
+// 2026-09-10 주석추가 및 스크립트 정리
 //  스와이퍼 실행하기
+// 메인배너
 var slider1 = new Swiper(".slider-1", {
     autoplay: {delay: 3000},
     loop: true,
@@ -7,8 +9,9 @@ var slider1 = new Swiper(".slider-1", {
         prevEl:".main .btn-left",
         nextEl:".main .btn-right",
     }
-})
+});
 
+// 섹션 3
 var slider2 = new Swiper(".slider-2", {
     autoplay: {delay: 3000},
     loop: true,
@@ -23,8 +26,9 @@ var slider2 = new Swiper(".slider-2", {
         768: {slidesPerView: 3},
         1024: {slidesPerView: 4}
     },
-})
+});
 
+// 섹션 9
 var slider3 = new Swiper(".slider-3", {
     autoplay: {delay: 3000},
     loop: true,
@@ -39,15 +43,23 @@ var slider3 = new Swiper(".slider-3", {
         768: {slidesPerView: 3},
         1024: {slidesPerView: 4}
     },
-})
+});
 
-// 팝업
+// 섹션 1 상품 팝업
 $(".section1 .item").click(function(){
-    pop_i = $(this).index()
-    $(".section1 .popup").stop().fadeIn()
-    $(".section1 .pop-box").stop().fadeOut()
-    $(".section1 .pop-box").eq(pop_i).stop().fadeIn()
-})
+    var pop_i = $(this).index();
+    $(".section1 .popup").stop().fadeIn();
+    $(".section1 .pop-box").stop().fadeOut();
+    $(".section1 .pop-box").eq(pop_i).stop().fadeIn();
+});
+
 $(".section1 .popup .bg").click(function(){
-    $(".section1 .popup").stop().fadeOut()
-})
+    $(".section1 .popup").stop().fadeOut();
+});
+
+// 2026-09-10 섹션 6 폼 스크립트 추가
+$(".section6 form").on("submit", function(e) {
+    e.preventDefault();
+    showAlert("문의요청 전송이 완료되었습니다.");
+    this.reset();
+});
